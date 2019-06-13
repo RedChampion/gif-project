d6ddd5c979717a1b9d60961d9c655301cfc7502e
Christian O'Keefe <okeefe.christian94@gmail.com>
2019-06-12 18:13:05 -0700

    Added more javascript

--- assets/java.js
+++ assets/java.js
@@ -5,40 +5,37 @@ function displayGifInfo() {
     var gif = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     gif + "&api_key=cWU8wqpkHwuAJRb1nTnWJD65Ds3u4fSW&limit=10";
-    // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        // Storing an array of results in the results variable
         var results = response.data;
 
-        // Looping over every result item
         for (var i = 0; i < results.length; i++) {
 
-          // Only taking action if the photo has an appropriate rating
           if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-            // Creating a div for the gif
             var gifDiv = $("<div>");
 
-            // Storing the result item's rating
             var rating = results[i].rating;
 
-            // Creating a paragraph tag with the result item's rating
             var p = $("<p>").text("Rating: " + rating);
 
-            // Creating an image tag
-            var gifImage = $("<img>");
-
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
+            var gifImage = $(`<img data-state="still" class="gif">`);
+            $(".gif").on("click", function() {
+              var state = $(this).attr("data-state");
+              if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+              } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+              }
+            });
             gifImage.attr("src", results[i].images.fixed_height.url);
 
-            // Appending the paragraph and personImage we created to the "gifDiv" div we created
             gifDiv.append(p);
             gifDiv.append(gifImage);
 
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
             $("#gif-view").prepend(gifDiv);
           }
         }
@@ -47,42 +44,32 @@ function displayGifInfo() {
   };
   function renderButtons() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
     $("#buttons-view").empty();
 
-    // Looping through the array of movies
     for (var i = 0; i < gifs.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
       var a = $("<button>");
-      // Adding a class of movie-btn to our button
+
       a.addClass("gif-btn");
-      // Adding a data-attribute
+
       a.attr("data-name", gifs[i]);
-      // Providing the initial button text
+
       a.text(gifs[i]);
-      // Adding the button to the buttons-view div
+
       $("#buttons-view").append(a);
     }
   }
 
-  // This function handles events where a movie button is clicked
   $("#add-gif").on("click", function(event) {
     event.preventDefault();
-    // This line grabs the input from the textbox
+
     var gif = $("#gif-input").val().trim();
 
-    // Adding movie from the textbox to our array
     gifs.push(gif);
 
-    // Calling renderButtons which handles the processing of our movie array
     renderButtons();
   });
 
-  // Adding a click event listener to all elements with a class of "movie-btn"
   $(document).on("click", ".gif-btn", displayGifInfo);
 
-  // Calling the renderButtons function to display the intial buttons
   renderButtons();
